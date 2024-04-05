426790438c640072c620232811465c1af00e1122
Kaitlin Cort <kaitlin.zhang@owasp.org>
2024-04-05 13:10:07 -0600

    Add Logger in logging package as globally accessible Singleton pattern

--- src/edu/upenn/cit594/logging/Logger.java
+++ src/edu/upenn/cit594/logging/Logger.java
@@ -1,4 +1,18 @@
 package edu.upenn.cit594.logging;
 
 public class Logger {
+    private static Logger instance;
+
+    private Logger() {}
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        // Implementation to log messages to a file
+    }
 }
