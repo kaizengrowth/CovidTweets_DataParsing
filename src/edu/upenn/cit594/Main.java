56cbae2e088d77d464d9a2672662241c136be133
Kaitlin Cort <kaitlin.zhang@owasp.org>
2024-04-05 13:33:26 -0600

    Error handling in Main: expect 3 command line arguments.

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,6 +1,32 @@
 package edu.upenn.cit594;
+import edu.upenn.cit594.util.Tweet;
+import edu.upenn.cit594.datamanagement.TweetReader;
+import edu.upenn.cit594.datamanagement.JSONTweetReader;
+import edu.upenn.cit594.datamanagement.TextTweetReader;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Error handling: expect 3 command line arguments
+        if (args.length != 3) {
+            System.err.println("Usage: java Main <tweets_file> <states_file> <log_file>");
+            System.exit(1);
+        }
+
+        String tweetsFile = args[0];
+        TweetReader reader;
+
+        if (tweetsFile.endsWith(".json")) {
+            reader = new JSONTweetReader();
+        } else if (tweetsFile.endsWith(".txt")) {
+            reader = new TextTweetReader();
+        } else {
+            System.err.println("Invalid tweets file format.");
+            System.exit(1);
+            return;
+        }
+
+        List<Tweet> tweets = reader.readTweets(tweetsFile);
     }
 }
